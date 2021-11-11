fdebed1c59382466ed775887730522b7f86ed0b3
Wojnstup <78437858+Wojnstup@users.noreply.github.com>
2021-11-11 18:48:34 +0000

    Update killer.sh

--- killer.sh
+++ killer.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-mode=$(echo -e "Select\nFast" | dmenu -p "Select kill mode" -sb '#98005d' -fn "Terminus:bold:size:15" -h 27 )
+mode=$(echo -e "Select\nFast" | dmenu -p "Select kill mode" -sb '#98005d' -fn "Terminus:bold:size:15" )
 
 if [[ $mode == "Select" ]]
 then
-	program=$(ps -u $USER -o %mem,comm| sed -n '1!p'  | sort -r | dmenu -i -sb '#98005d' -l 10 -fn "Terminus:bold:size:15" -h 27 )
+	program=$(ps -u $USER -o %mem,comm| sed -n '1!p'  | sort -r | dmenu -i -sb '#98005d' -l 10 -fn "Terminus:bold:size:15" )
 	program=$( echo "$program" | awk '{print $2}' )
 	pkill $program
 elif [[ $mode == "Fast" ]]
